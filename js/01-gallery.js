05a29930180607e6c1ed6ca96fb9b5bf9da68e9f
Beniamin Brzeziński <benek9311@wp.pl>
2023-02-17 12:38:41 +0100

    Created the main assumption of the task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryTags = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryTags);
+
+const onGalleryImgClick = (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImgOriginalSrc = e.target.dataset.source;
+  const originalImg = basicLightbox.create(`
+    <img src="${selectedImgOriginalSrc}" width="800" height="600">
+    `);
+
+  originalImg.show(document.addEventListener("keydown", closeWithEscape));
+
+  function closeWithEscape({ code }) {
+    if (code === "Escape") {
+      originalImg.close(
+        document.removeEventListener("keydown", closeWithEscape)
+      );
+    }
+  }
+};
+
+gallery.addEventListener("click", onGalleryImgClick);
