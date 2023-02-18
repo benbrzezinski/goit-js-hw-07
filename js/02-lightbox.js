f9836cd43249971842cfc47e05ebfdc1c76004ee
Beniamin Brzeziński <benek9311@wp.pl>
2023-02-18 17:50:57 +0100

    Completion of the second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryTags = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryTags);
+
+const gallerySlider = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
